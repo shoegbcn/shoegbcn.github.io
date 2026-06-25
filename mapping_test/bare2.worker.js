774d2415ba37cd489ba2d1ecdb78edfa3e7a4dc7
shoeg <shoegmusic@gmail.com>
2026-06-25 16:19:03 +0200

    diag

--- mapping_test/bare2.worker.js
+++ mapping_test/bare2.worker.js
@@ -6,7 +6,14 @@ self.addEventListener("message", async (e) => {
   console.log("[bare2] CREATED");
   const cfg = rt.getConfig();
   const ex = await rt.getAssemblyExports(cfg.mainAssemblyName);
-  console.log("[bare2] exports", Object.keys(ex.DiatomBloom || {}));
+  console.log("[bare2] exports", Object.keys(ex || {}), "DiatomBloom =", Object.keys(ex.DiatomBloom || {}));
+  const Engine = ex.DiatomBloom.Engine;
+  console.log("[bare2] Engine type =", typeof Engine);
+  console.log("[bare2] calling Init");
+  Engine.Init("alfacs", 0, 120);
+  console.log("[bare2] Init done");
+  const keys = Engine.GetSpeciesKeys();
+  console.log("[bare2] keys =", keys);
 });
 ({ dotnet } = await import(new URL("./_framework/dotnet.js", import.meta.url).href));
 console.log("[bare2] imported, ready for message");
